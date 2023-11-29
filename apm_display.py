4191b5916f888a86548ffd9fb4dc5d446e041c60
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-11-29 21:20:35 +0100

    initial commit

---
+++ apm_display.py
@@ -0,0 +1,80 @@
+import pyautogui
+import time
+import tkinter as tk
+from pynput import keyboard
+from datetime import datetime
+
+def get_stamp():
+    return time.time()
+
+last_alert = 0
+buffer = []
+
+def show_alert(message, timeout):
+    root = tk.Tk()
+    root.title("Alert")
+
+    label = tk.Label(root, text=message)
+    label.pack(padx=20, pady=20)
+
+    # Close the window after the specified timeout
+    root.after(timeout, root.destroy)
+
+    root.mainloop()
+
+def on_press(key):
+    global last_alert
+    global buffer
+
+    stamp = get_stamp()
+    buffer.append(stamp)
+    start_time = buffer[0]
+    num_keys = 100
+    window_len_s = 60
+    keys_pressed = len(buffer)
+    # Calculate APM based on the number of keystrokes (customize this based on your needs)
+    elapsed_time = stamp - start_time
+
+    if elapsed_time > 0:
+        apm = round(keys_pressed * 60 / elapsed_time)  # Calculate APM
+        #start_time = time.time()  # Reset the start time
+
+        first_not_2 = buffer[:-2]
+        last_2 = buffer[-2:]
+        new_buffer = [x for x in first_not_2 if stamp - x <= window_len_s]
+        new_buffer.extend(last_2)
+        buffer = new_buffer
+        #buffer = buffer[-num_keys:]
+
+        try:
+            # Print the key that was pressed
+            pass
+            #print(f'Key pressed: {key.char}')
+        except AttributeError:
+            # Handle special keys
+            pass
+            #print(f'Special key pressed: {key}')
+
+        print("APM", apm, buffer[0], buffer[-1], len(buffer))
+        dt_object = datetime.fromtimestamp(stamp)
+        formatted_time = dt_object.strftime("%Y-%m-%d %H:%M:%S")
+        logfile = open('log_of_apm.txt', 'a')
+        logfile.write(str(formatted_time) + ", " + str(stamp) + ': ' + str(apm) + '\n')
+        logfile.close()
+
+        # Update the on-screen display
+        #pyautogui.alert(text=f'APM: {apm}', title='APM Display', button='OK')
+
+        alert_elapsed = time.time() - last_alert
+        if alert_elapsed >= 5000:
+            #show_alert("APM: " + str(apm), 1000)
+            last_alert = time.time()
+
+def on_release(key):
+    # If the key 'Esc' is pressed, stop the listener
+    if key == keyboard.Key.esc:
+        return False
+
+# Collect events until released
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
